1befa438202278ae12e7168bc5dc92819625e63a
Thomas Kalka <thomas.kalka@gmail.com>
2025-02-22 13:35:35 +0100

    added more dice sets

---
+++ src/lib/DiceStats.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let dices = {};
+
+
+  import W2 from "$lib/W2.svelte";
+
+
+
+  let all_combinations = (dices) => {
+    let result = []
+    for (let i = 0; i < dices.length; i++) {
+      for (let j = i + 1; j < dices.length; j++) {
+        result.push([dices[i], dices[j]])
+      }
+    }
+    return result
+  }
+
+  let set = dices
+  
+</script>
+
+<main>
+  <h2>Set: {set.name}</h2>  
+
+  <div class="grid">
+    {#each all_combinations(set.dices,) as [diceA, diceB]}
+      <W2 A={diceA} B={diceB}></W2>
+    {/each}
+  </div>
+
+</main>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 20px;
+  }
+
+  main {
+    text-align: center;
+    padding: 20px;
+  }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import { afterNavigate } from "$app/navigation";
-	import W2 from "$lib/W2.svelte";
+	import DiceStats from "$lib/DiceStats.svelte";
 
   let grime = 
     { name: "Grime", dices: [
@@ -10,44 +10,57 @@
       { name: "Blau", sides: "222777", color: "blue" },
       { name: "Olive", sides: "055555", color: "green" }
     ]}
-  
-  // calc all possible pairs of a set of dices
 
-  let all_combinations = (dices) => {
-    let result = []
-    for (let i = 0; i < dices.length; i++) {
-      for (let j = i + 1; j < dices.length; j++) {
-        result.push([dices[i], dices[j]])
-      }
-    }
-    return result
-  }
+    let grime3 = 
+    { name: "Grime3", dices: [
+      { name: "Rot",   sides: "333336", color: "red" },
+      { name: "Blau",  sides: "222555", color: "blue" },
+      { name: "Olive", sides: "144444", color: "green" },
+    ]}
 
-  let set = grime
+    let efron = 
+    { name: "Efron", dices: [
+      { name: "Blau",    sides: "333333", color: "blue" },
+      { name: "Magenta", sides: "222266", color: "magenta" },
+      { name: "Olive",   sides: "111555", color: "green" },
+      { name: "Red",     sides: "004444", color: "red" },
+    ]}
+
+    let grime35 = 
+    { name: "Grime3+Grime5", dices: [
+      { name: "3Rot",   sides: "333336", color: "#f77" },
+      { name: "3Blau",  sides: "222555", color: "#77f" },
+      { name: "3Olive", sides: "144444", color: "lightgreen" },
+      { name: "Rot",  sides: "444449", color: "red" },
+      { name: "Gelb", sides: "333388", color: "yellow" },
+      { name: "Magenta", sides: "116666", color: "magenta" },
+      { name: "Blau", sides: "222777", color: "blue" },
+      { name: "Olive", sides: "055555", color: "green" }
+    ]}
+
   
 </script>
 
 <main>
-  <h1>Intransitive Würfel ({set.name})</h1>  
+  <h1>Intransitive Würfel</h1>
 
-  <div class="grid">
-    {#each all_combinations(set.dices,) as [diceA, diceB]}
-      <W2 A={diceA} B={diceB}></W2>
-    {/each}
-  </div>
+  <DiceStats dices={grime}></DiceStats>
+  <DiceStats dices={grime3}></DiceStats>
+  <DiceStats dices={efron}></DiceStats>
+  <DiceStats dices={grime35}></DiceStats>
 
 </main>
 
 <style>
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    gap: 20px;
-  }
-
   main {
     text-align: center;
-    padding: 20px;
+    
   }
 
+  h1 {
+    font-size: 2em;
+    margin: 0;
+    padding: 0;
+  }
+  
 </style>
